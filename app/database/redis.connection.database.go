f3bd616940776449112f51821b6f73d16fcc7411
Tolu Adesina <tolu.adesina79@gmail.com>
2023-07-16 21:52:59 +0200

    UPDATE redis database connection function description and config

--- app/database/redis.connection.database.go
+++ app/database/redis.connection.database.go
@@ -8,22 +8,28 @@ import (
 	"time"
 
 	"github.com/redis/go-redis/v9"
-)
-
-const (
-	redisUser     = ""
-	redisHost     = "localhost"
-	redisPort     = "6379"
-	redisPassword = ""
-	redisDatabase = 0
+	config "github.com/tolubydesign/angular-story-backend/app/config"
 )
 
 var redisClientSingleton *redis.Client
 
-// Create and connect to redis database
-// How To Start Logging With Redis - https://betterstack.com/community/guides/logging/how-to-start-logging-with-redis/
+/*
+Create and connect to redis database.
+
+Resource: How To Start Logging With Redis - https://betterstack.com/community/guides/logging/how-to-start-logging-with-redis/
+
+Returning redis client and possible error.
+*/
 func ConnectToRedisDatabase() (*redis.Client, error) {
-	databaseAddress := fmt.Sprintf("redis://%v:%v@%v:%v/%v", redisUser, redisPassword, redisHost, redisPort, redisDatabase)
+	var err error
+	config, err := config.GetConfiguration()
+	if err != nil {
+		return nil, err
+	}
+
+	var redisConfig = config.Configuration.Redis
+
+	databaseAddress := fmt.Sprintf("redis://%v:%v@%v:%v/%v", redisConfig.User, redisConfig.Password, redisConfig.Host, redisConfig.Port, redisConfig.Database)
 	opt, err := redis.ParseURL(databaseAddress)
 	if err != nil {
 		return nil, err
