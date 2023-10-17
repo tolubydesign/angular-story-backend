0087a10d15ca89ea06f30f34f92b546dc2bf30e2
Tolu Adesina <tolu.adesina79@gmail.com>
2023-10-18 01:01:30 +0200

    ADD put item to dynamodb request

--- app/models/story.model.go
+++ app/models/story.model.go
@@ -4,19 +4,27 @@ import (
 	"github.com/google/uuid"
 )
 
+type Story struct {
+	Id          uuid.UUID   `json:"id" validate:"uuid"`
+	Title       string      `json:"title" validate:"required"`
+	Description string      `json:"description" validate:"required"`
+	Content     interface{} `json:"content"` // type *StoryContent
+}
+
 type StoryContent struct {
 	Id          string          `json:"id"`
 	Name        string          `json:"name"`
 	Description string          `json:"description"`
 	Children    *[]StoryContent `json:"children"`
 }
-type Story struct {
-	Id          uuid.UUID   `json:"id" validate:"uuid"`
-	Title       string     `json:"title" validate:"required"`
-	Description string     `json:"description" validate:"required"`
-	Content     interface{} `json:"content"` // type *StoryContent
-}
 
 type AllStories struct {
 	Story []Story `json:"story"`
+}
+
+type DynamoStoryStruct struct {
+	Id          string      `dynamodbav:"id"`
+	Title       string      `dynamodbav:"title"`
+	Description string      `dynamodbav:"description"`
+	Content     interface{} `dynamodbav:"content"`
 }
