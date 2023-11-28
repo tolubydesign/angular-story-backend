a07232f9bd67ecbb17188ddbe15981d8980e3e0d
Tolu Adesina <tolu.adesina79@gmail.com>
2023-11-28 20:49:54 +0200

    BUILD cdk

--- cdk/cdk-golang.go
+++ cdk/cdk-golang.go
@@ -2,63 +2,218 @@ package cdk
 
 import (
 	configuration "github.com/tolubydesign/angular-story-backend/app/config"
-	
+
 	"github.com/aws/aws-cdk-go/awscdk/v2"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsapigateway"
-	// "github.com/aws/aws-cdk-go/awscdk/awsapigatewayv2"
-	"github.com/aws/aws-cdk-go/awscdk/awsapigatewayv2integrations"
-	"github.com/aws/aws-cdk-go/awscdk/awslambdago"
+
 	// "github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
+
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsdynamodb"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsiam"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awslogs"
+	// "apigtw-lambda-ddb/config"
 )
+
+// resource - https://harshq.medium.com/building-apps-with-aws-sdk-for-golang-api-gateway-and-lambda-b254858b1d71
+// resource - https://docs.aws.amazon.com/apigateway/latest/developerguide/http-api-dynamo-db.html#http-api-dynamo-db-create-table
+// resource - https://pkg.go.dev/github.com/aws/aws-cdk-go/awscdk/awsapigateway#section-readme
+// resource - https://pkg.go.dev/github.com/aws/aws-cdk-go/awscdk/awsdynamodb
+// (!) resource - https://github.com/aws-samples/serverless-patterns/tree/main/apigw-lambda-dynamodb-cdk-go
 
 type CdkGolangStackProps struct {
 	awscdk.StackProps
 }
-
-/**
- * This file showcases how to split up a RestApi's Resources and Methods across nested stacks.
- *
- * The root stack 'RootStack' first defines a RestApi.
- * Two nested stacks BooksStack and PetsStack, create corresponding Resources '/books' and '/pets'.
- * They are then deployed to a 'prod' Stage via a third nested stack - DeployStack.
- *
- * To verify this worked, go to the APIGateway
- */
-type rootStack struct {
-	stack
+type ApiGtwLambdaDdbStackProps struct {
+	awscdk.StackProps
 }
 
 func NewCdkGolangStack(scope constructs.Construct, id string, props *CdkGolangStackProps) awscdk.Stack {
-	this := &rootStack{}
-	var backend function
 	var sprops awscdk.StackProps
+
+	c, err := configuration.GetConfiguration()
+	storyDynamoDBTable := c.Configuration.Dynamodb.StoryTableName
+	// userDynamoDBTable := c.Configuration.Dynamodb.UserTableName
+	if err != nil {
+		panic(err)
+	}
+
 	if props != nil {
 		sprops = props.StackProps
 	}
 	stack := awscdk.NewStack(scope, &id, &sprops)
 
-	// The code that defines your stack goes here
-	// Create a new api HTTP api on gateway v2.
-	api := awsapigateway.NewRestApi(stack, jsii.String("cdk-lambda-api"), &awsapigateway.RestApiProps{
-		RestApiName: jsii.String("cdk-lambda-api"),
-		
-	})
-	// api = awsapigateway.NewHttpApi(stack, jsii.String("cdk-lambda-api"), &awsapigatewayv2.HttpApiProps{
-	// 	CorsPreflight: &awsapigateway.CorsPreflightOptions{
-	// 		AllowOrigins: &[]*string{jsii.String("*")}, //
-	// 		AllowMethods: &[]awsapigateway.CorsHttpMethod{awsapigatewayv2.CorsHttpMethod_ANY},
-	// 	},
-	// })
+	sourceDynamoDBTable := awsdynamodb.NewTable(stack, jsii.String("Story"),
+		&awsdynamodb.TableProps{
+			PartitionKey: &awsdynamodb.Attribute{
+				Name: jsii.String("email"),
+				Type: awsdynamodb.AttributeType_STRING},
+			Stream: awsdynamodb.StreamViewType_NEW_AND_OLD_IMAGES})
 
+	// Create DynamoDB Base table.
+	// Data Modeling
+	// name(PK), time(SK),                  comment, chat_room
+	// string    string(micro sec unixtime)	string   string
+	chatTable := awsdynamodb.NewTable(stack, jsii.String(storyDynamoDBTable), &awsdynamodb.TableProps{
+		TableName:     jsii.String(*stack.StackName() + "-" + config.DynamoDBTable),
+		BillingMode:   awsdynamodb.BillingMode_PAY_PER_REQUEST,
+		ReadCapacity:  jsii.Number(1),
+		WriteCapacity: jsii.Number(1),
+		RemovalPolicy: awscdk.RemovalPolicy_DESTROY,
+		PartitionKey: &awsdynamodb.Attribute{
+			Name: jsii.String("name"),
+			Type: awsdynamodb.AttributeType_STRING,
+		},
+		SortKey: &awsdynamodb.Attribute{
+			Name: jsii.String("time"),
+			Type: awsdynamodb.AttributeType_STRING,
+		},
+		PointInTimeRecovery: jsii.Bool(true),
+	})
+
+	// Create role for lambda function.
+	lambdaRole := awsiam.NewRole(stack, jsii.String("LambdaRole"), &awsiam.RoleProps{
+		RoleName:  jsii.String(*stack.StackName() + "-LambdaRole"),
+		AssumedBy: awsiam.NewServicePrincipal(jsii.String("lambda.amazonaws.com"), nil),
+		ManagedPolicies: &[]awsiam.IManagedPolicy{
+			awsiam.ManagedPolicy_FromAwsManagedPolicyName(jsii.String("AmazonDynamoDBFullAccess")),
+			awsiam.ManagedPolicy_FromAwsManagedPolicyName(jsii.String("CloudWatchFullAccess")),
+		},
+	})
+
+	// Create put-chat-records function.
+	putFunction := awslambda.NewFunction(stack, jsii.String("PutFunction"), &awslambda.FunctionProps{
+		FunctionName: jsii.String(*stack.StackName() + "-PutChatRecords"),
+		Runtime:      awslambda.Runtime_GO_1_X(),
+		MemorySize:   jsii.Number(128),
+		Timeout:      awscdk.Duration_Seconds(jsii.Number(60)),
+		Code:         awslambda.AssetCode_FromAsset(jsii.String("functions/put-chat-records/."), nil),
+		Handler:      jsii.String("put-chat-records"),
+		Architecture: awslambda.Architecture_X86_64(),
+		Role:         lambdaRole,
+		LogRetention: awslogs.RetentionDays_ONE_WEEK,
+		Environment: &map[string]*string{
+			"DYNAMODB_TABLE": jsii.String(*stack.StackName() + "-" + config.DynamoDBTable),
+		},
+	})
+
+	// Create get-chat-records function.
+	getFunction := awslambda.NewFunction(stack, jsii.String("GetChatRecords"), &awslambda.FunctionProps{
+		FunctionName: jsii.String(*stack.StackName() + "-GetChatRecords"),
+		Runtime:      awslambda.Runtime_GO_1_X(),
+		MemorySize:   jsii.Number(128),
+		Timeout:      awscdk.Duration_Seconds(jsii.Number(60)),
+		Code:         awslambda.AssetCode_FromAsset(jsii.String("functions/get-chat-records/."), nil),
+		Handler:      jsii.String("get-chat-records"),
+		Architecture: awslambda.Architecture_X86_64(),
+		Role:         lambdaRole,
+		LogRetention: awslogs.RetentionDays_ONE_WEEK,
+		Environment: &map[string]*string{
+			"DYNAMODB_TABLE": jsii.String(*stack.StackName() + "-" + config.DynamoDBTable),
+			"DYNAMODB_GSI":   jsii.String(config.DynamoDBGSI),
+		},
+		// ReservedConcurrentExecutions: jsii.Number(1),
+	})
+
+	// Create API Gateway rest api.
+	restApi := awsapigateway.NewRestApi(stack, jsii.String("LambdaRestApi"), &awsapigateway.RestApiProps{
+		RestApiName:        jsii.String(*stack.StackName() + "-LambdaRestApi"),
+		RetainDeployments:  jsii.Bool(false),
+		EndpointExportName: jsii.String("RestApiUrl"),
+		Deploy:             jsii.Bool(true),
+		DeployOptions: &awsapigateway.StageOptions{
+			StageName:           jsii.String("dev"),
+			CacheClusterEnabled: jsii.Bool(true),
+			CacheClusterSize:    jsii.String("0.5"),
+			CacheTtl:            awscdk.Duration_Minutes(jsii.Number(1)),
+			// https://www.petefreitag.com/item/853.cfm
+			// This can help you better understand what burst and rate limite are.
+			ThrottlingBurstLimit: jsii.Number(100),
+			ThrottlingRateLimit:  jsii.Number(1000),
+		},
+	})
+
+	// Add path resources to rest api.
+	// You MUST associate ApiKey with the methods for the UsagePlane to work.
+	putRecordsRes := restApi.Root().AddResource(jsii.String("put-chat-records"), nil)
+	putRecordsRes.AddMethod(jsii.String("POST"), awsapigateway.NewLambdaIntegration(putFunction, nil), &awsapigateway.MethodOptions{
+		ApiKeyRequired: jsii.Bool(true),
+	})
+	getRecordsRes := restApi.Root().AddResource(jsii.String("get-chat-records"), nil)
+	getMethod := getRecordsRes.AddMethod(jsii.String("GET"), awsapigateway.NewLambdaIntegration(getFunction, nil), &awsapigateway.MethodOptions{
+		ApiKeyRequired: jsii.Bool(true),
+	})
+
+	// UsagePlane's throttle can override Stage's DefaultMethodThrottle,
+	// while UsagePlanePerApiStage's throttle can override UsagePlane's throttle.
+	usagePlane := restApi.AddUsagePlan(jsii.String("UsagePlane"), &awsapigateway.UsagePlanProps{
+		Name: jsii.String(*stack.StackName() + "-UsagePlane"),
+		Throttle: &awsapigateway.ThrottleSettings{
+			BurstLimit: jsii.Number(10),
+			RateLimit:  jsii.Number(100),
+		},
+		Quota: &awsapigateway.QuotaSettings{
+			Limit:  jsii.Number(100),
+			Offset: jsii.Number(0),
+			Period: awsapigateway.Period_DAY,
+		},
+		ApiStages: &[]*awsapigateway.UsagePlanPerApiStage{
+			{
+				Api:   restApi,
+				Stage: restApi.DeploymentStage(),
+				Throttle: &[]*awsapigateway.ThrottlingPerMethod{
+					{
+						Method: getMethod,
+						Throttle: &awsapigateway.ThrottleSettings{
+							BurstLimit: jsii.Number(1),
+							RateLimit:  jsii.Number(10),
+						},
+					},
+				},
+			},
+		},
+	})
+
+	// Create ApiKey and associate it with UsagePlane.
+	apiKey := restApi.AddApiKey(jsii.String("ApiKey"), &awsapigateway.ApiKeyOptions{})
+	usagePlane.AddApiKey(apiKey, &awsapigateway.AddApiKeyOptions{})
+
+	// Create DynamoDB GSI table.
+	// Data Modeling
+	// chat_room(PK), time(SK),                  comment, name
+	// string         string(micro sec unixtime) string   string
+	chatTable.AddGlobalSecondaryIndex(&awsdynamodb.GlobalSecondaryIndexProps{
+		IndexName: jsii.String(config.DynamoDBGSI),
+		PartitionKey: &awsdynamodb.Attribute{
+			Name: jsii.String("chat_room"),
+			Type: awsdynamodb.AttributeType_STRING,
+		},
+		SortKey: &awsdynamodb.Attribute{
+			Name: jsii.String("time"),
+			Type: awsdynamodb.AttributeType_STRING,
+		},
+		ProjectionType: awsdynamodb.ProjectionType_ALL,
+	})
+
+	// Grant access to lambda functions.
+	chatTable.GrantWriteData(putFunction)
+	chatTable.GrantReadData(getFunction)
+
+	return stack
+
+	// var sprops awscdk.StackProps
+	// if props != nil {
+	// 	sprops = props.StackProps
+	// }
+	// stack := awscdk.NewStack(scope, &id, &sprops)
 	// The code that defines your stack goes here
 	// example resource
 	// queue := awssqs.NewQueue(stack, jsii.String("CdkGolangQueue"), &awssqs.QueueProps{
 	// 	VisibilityTimeout: awscdk.Duration_Seconds(jsii.Number(300)),
 	// })
-
-	return stack
+	// return stack
 }
 
 // env determines the AWS environment (account+region) in which our stack is to
