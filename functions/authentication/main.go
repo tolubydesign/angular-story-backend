c164a2128acb3054e6bbaeaa2315095c02beb555
Tolu Adesina <tolu.adesina79@gmail.com>
2024-01-15 00:28:36 +0200

    ADD SAM aws authentication function api and SAM deployment

--- functions/authentication/main.go
+++ functions/authentication/main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"context"
 	"errors"
-	"fmt"
 	"strings"
+
+	// "fmt"
 
 	// "log"
 	"os"
@@ -39,41 +40,77 @@ func HandleRequest(ctx context.Context, request events.APIGatewayCustomAuthorize
 	if len(tokenSlice) > 1 {
 		bearerToken = tokenSlice[len(tokenSlice)-1]
 	}
-
-	fmt.Println("context", ctx)
-	fmt.Println("request", request)
-
-	// Check that user has provided the correct authentication key
-	if apiKey == "" {
-		return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Api Key held by system")
-	}
-
 	if bearerToken != apiKey {
 		return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Authentication key provide is invalid")
 	}
-
-	// awsConfig, err := config.LoadDefaultConfig(context.TODO())
-	// if err != nil {
-	// 	log.Println("aws config failed to load.", err)
-
-	// 	return events.APIGatewayV2CustomAuthorizerSimpleResponse{
-	// 		IsAuthorized: false,
-	// 	}, fmt.Errorf("Invalid configuration.") // OR }, nil
-	// }
-
-	// if request.Headers["Authorization"] == "" {
-	// 	return events.APIGatewayCustomAuthorizerResponse{}, errors.New("No authentication key found.")
-	// }
-
-	// if apiKey != request.Headers["Authorization"] {
-	// 	return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Authentication key provide is invalid.")
-	// }
-
-	// return events.APIGatewayV2CustomAuthorizerSimpleResponse{
-	// 	IsAuthorized: true,
-	// }, nil
 	return generatePolicy("user", "Allow", request.MethodArn), nil
+
+	//
+	// /////...............
+	//
+
+	// var bearerToken string
+	// if len(tokenSlice) > 1 {
+	// 	bearerToken = tokenSlice[len(tokenSlice)-1]
+	// }
+
+	// fmt.Println("context", ctx)
+	// fmt.Println("request", request)
+
+	// // Check that user has provided the correct authentication key
+	// if apiKey == "" {
+	// 	return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Api Key held by system")
+	// }
+
+	// if bearerToken != apiKey {
+	// 	return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Authentication key provide is invalid")
+	// }
+
+	// ***************
+	// return generatePolicy("user", "Allow", request.MethodArn), nil
 }
+
+// *************** below is useful
+// // func HandleRequest(ctx context.Context, request events.APIGatewayCustomAuthorizerRequestTypeRequest) (events.APIGatewayCustomAuthorizerResponse, error) {
+// func HandleRequest(ctx context.Context, request events.APIGatewayCustomAuthorizerRequestTypeRequest) (events.APIGatewayV2CustomAuthorizerSimpleResponse, error) {
+// 	// awsConfig, err := config.LoadDefaultConfig(context.TODO())
+// 	// if err != nil {
+// 	// 	log.Println("aws config failed to load.", err)
+
+// 	// 	return events.APIGatewayV2CustomAuthorizerSimpleResponse{
+// 	// 		IsAuthorized: false,
+// 	// 	}, fmt.Errorf("Invalid configuration.") // OR }, nil
+// 	// }
+// 	if apiKey == "" {
+// 		return events.APIGatewayV2CustomAuthorizerSimpleResponse{}, errors.New("Invalid configuration")
+// 	}
+// 	// https://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-lambda-authorizer-output.html
+// 	// https://docs.aws.amazon.com/apigateway/latest/developerguide/http-api-lambda-authorizer.html
+// 	context := map[string]interface{}{
+// 		"ctx":     ctx,
+// 		"request": request,
+// 	}
+
+// 	auth := request.Headers["Authorization"]
+// 	if auth == "" {
+// 		return events.APIGatewayV2CustomAuthorizerSimpleResponse{
+// 			IsAuthorized: false,
+// 			Context:      context,
+// 		}, errors.New("No authentication key found")
+// 	}
+
+// 	if apiKey != auth {
+// 		return events.APIGatewayV2CustomAuthorizerSimpleResponse{
+// 			IsAuthorized: false,
+// 			Context:      context,
+// 		}, errors.New("Authentication key provide is invalid")
+// 	}
+
+// 	return events.APIGatewayV2CustomAuthorizerSimpleResponse{
+// 		IsAuthorized: true,
+// 		Context:      context,
+// 	}, nil
+// }
 
 // func HandleRequest(request events.APIGatewayCustomAuthorizerRequest) (events.APIGatewayCustomAuthorizerResponse, error) {
 // 	token := request.AuthorizationToken
@@ -89,9 +126,8 @@ func HandleRequest(ctx context.Context, request events.APIGatewayCustomAuthorize
 // 	return generatePolicy("user", "Allow", request.MethodArn), nil
 // }
 
-func generatePolicy(principalID, effect, resource string) events.APIGatewayCustomAuthorizerResponse {
+func generatePolicy(principalID string, effect string, resource string) events.APIGatewayCustomAuthorizerResponse {
 	authResponse := events.APIGatewayCustomAuthorizerResponse{PrincipalID: principalID}
-
 	if effect != "" && resource != "" {
 		authResponse.PolicyDocument = events.APIGatewayCustomAuthorizerPolicy{
 			Version: "2012-10-17",
@@ -104,5 +140,12 @@ func generatePolicy(principalID, effect, resource string) events.APIGatewayCusto
 			},
 		}
 	}
+
+	// Optional output with custom properties of the String, Number or Boolean type.
+	authResponse.Context = map[string]interface{}{
+		"resource": resource,
+		"effect":   effect,
+	}
+
 	return authResponse
 }
