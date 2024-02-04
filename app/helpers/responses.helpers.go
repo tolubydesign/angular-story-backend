6009376156258afcce793a49280e18ce95f68950
Tolu Adesina <tolu.adesina79@gmail.com>
2024-02-04 10:47:44 +0200

    ADD alternative request response message variables

--- app/helpers/responses.helpers.go
+++ app/helpers/responses.helpers.go
@@ -3,7 +3,13 @@ package helpers
 import "github.com/tolubydesign/angular-story-backend/app/models"
 
 var ResponseMessages = models.ResponseMessage{
-	NilClient:        "Dynamo Database inaccessible.",
-	InvalidUUID:      "Invalid ID provided.",
-	InvalidCreatorID: "Invalid creator value provided.",
+	NilClient:        "Dynamo Database inaccessible",
+	InvalidUUID:      "Invalid ID provided",
+	InvalidCreatorID: "Invalid creator value provided",
 }
+
+const (
+	ResponseError_NilClient        = "Dynamo Database inaccessible"
+	ResponseError_InvalidUuid      = "Invalid ID provided"
+	ResponseError_InvalidCreatorID = "Invalid creator value provided"
+)
